2b1ed5b8344d30e293be9579e63cb5df570c8d94
xavierace990 <xavierparzival@gmail.com>
2024-05-10 09:42:48 -0700

    settings.py

--- project/settings.py
+++ project/settings.py
@@ -48,8 +48,8 @@ AUTHENTICATION_BACKENDS = [
     'django.contrib.auth.backends.ModelBackend',
 ]
 
-#SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = '113168208501-171j8evurut56bss470a5dh34htlf4rl.apps.googleusercontent.com'
-#SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = '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'
+SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = '113168208501-171j8evurut56bss470a5dh34htlf4rl.apps.googleusercontent.com'
+SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = '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'
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
